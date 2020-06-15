56886b44aca06876ff32a9e37dd9140969b1a47a
Adrián García <dev@agaman.me>
2020-06-16 01:34:12 +0200

    Change login test argument provider to CsvSource

--- backend/src/test/kotlin/me/agaman/ramona/MainRouterIntegrationTest.kt
+++ backend/src/test/kotlin/me/agaman/ramona/MainRouterIntegrationTest.kt
@@ -8,13 +8,9 @@ import io.ktor.server.testing.setBody
 import me.agaman.ramona.route.Route
 import me.agaman.ramona.test.RamonaIntegrationTest
 import me.agaman.ramona.test.TestCsrfTokenProvider
-import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.ArgumentsProvider
-import org.junit.jupiter.params.provider.ArgumentsSource
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
-import java.util.stream.Stream
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
@@ -31,17 +27,13 @@ internal class MainRouterIntegrationTest : RamonaIntegrationTest() {
         }
     }
 
-    class PostLoginArgumentsProvider : ArgumentsProvider {
-        override fun provideArguments(context: ExtensionContext?) = Stream.of(
-            Arguments.of("invalid-csrf", "testing", HttpStatusCode.Forbidden, null),
-            Arguments.of(TestCsrfTokenProvider.CSRF_TOKEN, "invalid_password", HttpStatusCode.Unauthorized, null),
-            Arguments.of(TestCsrfTokenProvider.CSRF_TOKEN, "testing", HttpStatusCode.OK, TestCsrfTokenProvider.CSRF_TOKEN)
-        )
-    }
-
     @ParameterizedTest
-    @ArgumentsSource(PostLoginArgumentsProvider::class)
-    fun `post login`(csrf: String, password: String, expectedResponseStatus: HttpStatusCode, expectedResponseBody: String?) {
+    @CsvSource(value = [
+        "invalid-csrf,                        testing,          403, ",
+        "${TestCsrfTokenProvider.CSRF_TOKEN}, invalid_password, 401, ",
+        "${TestCsrfTokenProvider.CSRF_TOKEN}, testing,          200, ${TestCsrfTokenProvider.CSRF_TOKEN}"
+    ])
+    fun `post login`(csrf: String, password: String, expectedHttpStatusCode: Int, expectedResponseBody: String?) {
         withKtorApp {
             handleRequest(HttpMethod.Post, Route.LOGIN.path) {
                 addHeader("X-CSRF", csrf)
@@ -55,7 +47,7 @@ internal class MainRouterIntegrationTest : RamonaIntegrationTest() {
                     ).bytes()
                 )
             }.apply {
-                assertEquals(expectedResponseStatus, response.status())
+                assertEquals(HttpStatusCode.fromValue(expectedHttpStatusCode), response.status())
                 assertEquals(expectedResponseBody, response.content)
             }
         }
